d7543eada6525353ec5aca56ba131858c05c5b3f
Muhammad Yaqoob <67253341+yaqoobwebhr@users.noreply.github.com>
2021-09-10 10:23:56 +0500

    Update register.js

--- register.js
+++ register.js
@@ -1,9 +1,9 @@
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("Face_Recognition/"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("Face_Recognition/"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("Face_Recognition/"),
-  faceapi.nets.faceExpressionNet.loadFromUri("Face_Recognition/"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("Face_Recognition/"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/Face_Recognition"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/Face_Recognition"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/Face_Recognition"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/Face_Recognition"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/Face_Recognition"),
 ]).then(() => console.log("Face API is ready!"));
 
 const Notifier = getNotifier();
