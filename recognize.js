903dfd615680671a611a3d06bcaa43482e3f94bd
Muhammad Yaqoob <67253341+yaqoobwebhr@users.noreply.github.com>
2021-09-14 15:53:41 +0500

    Update recognize.js

--- recognize.js
+++ recognize.js
@@ -54,7 +54,7 @@ async function startup(faces) {
     const detections = await faceapi
       .detectAllFaces(
         video,
-        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.7 })
+        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.8 })
       )
       .withFaceLandmarks()
       .withFaceDescriptors();
