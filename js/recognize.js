12b0a3b806997a123951d1c4090d764b519fb503
Nasir Abbasi <78782218+NasirAbbasiWebHR@users.noreply.github.com>
2021-09-28 15:51:48 +0500

    Testing

--- js/recognize.js
+++ js/recognize.js
@@ -42,7 +42,7 @@ async function startup(faces) {
     .flat()
     .map((item) => faceapi.LabeledFaceDescriptors.fromJSON(item));
 
-  faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.9);
+  faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
   setInterval(async function () {
     document.querySelectorAll(".canvas-result").forEach((el) => el.remove());
