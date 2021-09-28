dc84e542d1426f83e8960a3d214970d0ec581b70
Nasir Abbasi <78782218+NasirAbbasiWebHR@users.noreply.github.com>
2021-09-28 12:08:34 +0500

    Testing

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ const observer = new MutationObserver(async function (mutationRecords) {
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections, 0.05);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections, 0.5);
       }, 100);
     }
   } else {
