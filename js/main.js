74694dcd54772c9bcedcabf044832920811c3148
Nasir Abbasi <78782218+NasirAbbasiWebHR@users.noreply.github.com>
2021-09-28 11:19:26 +0500

    Testing

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ const recognizeObserver = new MutationObserver(async function (
       faceapi.LabeledFaceDescriptors.fromJSON(item)
     );
 
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.9);
+    // const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
     const videoTrack = recognizeFaceStream.getVideoTracks()[0];
     const imageCapture = new ImageCapture(videoTrack);
@@ -82,6 +82,14 @@ const recognizeObserver = new MutationObserver(async function (
           displaySize
         );
 
+        const results2 = await faceapi
+          .detectAllFaces(image)
+          .withFaceLandmarks()
+          .withFaceDescriptors();
+        if (!results2.length) {
+          return;
+        }
+        const faceMatcher = new faceapi.FaceMatcher(results2);
         const results = resizedDetections.map((d) =>
           faceMatcher.findBestMatch(d.descriptor)
         );
