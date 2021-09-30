5db97746da0211ea1ed0bedaca0fa2c5cbef0e88
Nasir Abbasi <78782218+NasirAbbasiWebHR@users.noreply.github.com>
2021-09-30 18:01:03 +0500

    Testing

--- js/recognize.js
+++ js/recognize.js
@@ -85,7 +85,7 @@ async function startup(faces) {
     if (payload.length > 0) Emitter.emit(Events.FACE_FOUND, { data: payload });
 
     results.forEach((result, i) => {
-      testing.innerHTML = results.LabeledFaceDescriptors;
+      testing.innerHTML = result.gender;
       const box = resizedDetections[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, {
         label: result.toString(),
