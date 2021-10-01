c5c800f3f8617fb0fd169be3e07fb6a54727975b
Nasir Abbasi <78782218+NasirAbbasiWebHR@users.noreply.github.com>
2021-10-01 10:01:31 +0500

    Testing

--- js/recognize.js
+++ js/recognize.js
@@ -86,7 +86,7 @@ async function startup(faces) {
     const payload = results.map((item) => ({
       label: item.label,
       distance: item.distance,
-      // ...payload2,
+      ...payload2,
     }));
 
     if (payload.length > 0) Emitter.emit(Events.FACE_FOUND, { data: payload });
